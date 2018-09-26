776acad390b7eb12b9fe4c48a534022d15c853e2
Joaquín García Lanzas <jgarcia@dapda.com>
2018-09-26 12:42:06 +0200

    edit post

--- pocmailgunpython.py
+++ pocmailgunpython.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/newlead')
-def pocmailgunpython(request):
+def pocmailgunpython():
     data = None
-    if request.method == 'POST':
-        data = request.POST
-        return data
+    #if request.method == 'POST':
+    #    data = request.POST
+    #    return data*/
     return 'Hello World'
