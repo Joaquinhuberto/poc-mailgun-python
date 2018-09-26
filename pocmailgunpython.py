f9020b1dc5231354d485f746e6b2d2ae918f6b2c
Joaquín García Lanzas <jgarcia@dapda.com>
2018-09-26 12:35:29 +0200

    add post

--- pocmailgunpython.py
+++ pocmailgunpython.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def pocmailgunpython():
+@app.route('/newdealer/')
+def pocmailgunpython(request, dealerid):
+    data = None
+    if request.method == 'POST':
+        data = request.POST
+        return data
     return 'Hello World'
